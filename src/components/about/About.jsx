470ee1eb0a13be8aadc372aee23d708296c7eaab
Jonathan Araldi <146822518+Skies-Land@users.noreply.github.com>
2025-04-22 21:11:21 +0200

    Mise à jour du lien du CV dans le composant About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -69,7 +69,7 @@ const About = () => {
               <a href="#contact" className="button">
                 <i className="ri-send-plane-line"></i> Contactez-moi
               </a>
-              <a href="https://cv-jonathan-araldi.web.app/" target="_blank" className="button" rel="noopener noreferrer">
+              <a href="https://drive.google.com/file/d/1zP7QVv1O0r1b-6_PsAERc2Ejw0HUx-bL/view?usp=sharing" target="_blank" className="button" rel="noopener noreferrer">
                 <i className="ri-download-line"></i> Mon CV
               </a>
               <a
